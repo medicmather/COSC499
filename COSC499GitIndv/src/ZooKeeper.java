3ef73f8d722a20f75401d92ca0bb7acc3969d2e7
medicmather <27987220+medicmather@users.noreply.github.com>
2022-09-16 19:01:57 -0700

    ZooKeeper species commit

--- COSC499GitIndv/src/ZooKeeper.java
+++ COSC499GitIndv/src/ZooKeeper.java
@@ -1,4 +1,22 @@
 
 public class ZooKeeper {
+	private int age;
+    private String name;
+    private String[] speciesKeeper;
+    
+
+    public ZooKeeper(int age, String name, String[] speciesKeeper){
+        this.age = age;
+        this.name = name;
+        this.speciesKeeper = speciesKeeper; 
+    }
+    
+    public int getAge(){
+        return age;
+    }
+
+    public String[] getSpeciesKeeper(){
+        return speciesKeeper;
+    }
 
 }
