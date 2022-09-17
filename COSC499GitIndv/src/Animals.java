0f4e51b248df907648d9bed2fa48ed95a91e5a5e
medicmather <27987220+medicmather@users.noreply.github.com>
2022-09-16 18:59:48 -0700

    Animals class commit

--- COSC499GitIndv/src/Animals.java
+++ COSC499GitIndv/src/Animals.java
@@ -1,4 +1,24 @@
 
 public class Animals {
+    private int age;
+    private String name;
+    private String[] species;
 
+    public Animals(int age,String name, String[] species){
+        this.age = age;
+        this.name = name;
+        this.species = species;
+    }
+
+    public String[] getSpecies(){
+        return species;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+    	return name;
+    }
 }
